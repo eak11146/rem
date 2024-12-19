d58c3e3af72fc2574e5e2a9e7b98997f4d039614
eak11146 <evo008hp@gmail.com>
2024-12-19 12:38:41 +0700

    set google fonts

---
+++ react-mongodb/src/component/ParallaxText.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ParallaxText = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ParallaxText
--- react-mongodb/src/page/home.jsx
+++ react-mongodb/src/page/home.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React,{ useRef } from 'react'
+ 
+import mernLogo from '../assets/mernstack.png'
+import imageMid from '../assets/images/parallax-demon-woods-mid-trees.png';
+
 
 const Home = () => {
+
+   
   return (
     <>
     <div className='container mx-auto justify-center md:h-[unset] md:max-w-[95%] lg:h-[100vh] lg:max-w-[70%] p-3'>
-       <h1 className="text-3xl font-bold underline bg-slate-500">
-      Hello world! Home
-    </h1>
+       <h1 className="text-3xl text-green-me font-normal">
+       Hi There, I'm  Eakawee
+    </h1> 
+    <h2>
+      Web Developer & Designer
+    </h2>    
+    <div className='bg-green-css rounded-xl  border-2 justify-items-center p-3'>
+      <p className='my-3'>
+      I'm a passionate web developer with expertise in React, Next.js, PHP ,CSS HTML JQUERY and Nodejs mysql mongodb ..., and modern web technologies. I love creating beautiful and functional website that solve real world problems.
+      </p>
+      
+    </div>
+    
+
+    <div className='grid justify-items-center w-full my-3'>
+      <img src={ mernLogo } style={{ height:'60px'}} className=''/>
+    </div>
+ 
 
     </div>
     </>
